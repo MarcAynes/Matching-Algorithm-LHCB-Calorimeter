f575c160a3af519e8068de694b30a08d6cb219bc
Marc Aynes <marcaynes2@gmail.com>
2022-03-03 16:43:40 +0100

    add: insert function

--- RTree.h
+++ RTree.h
@@ -1,9 +1,27 @@
 #ifndef MATCHINGALGORITHM_RTREE_H
 #define MATCHINGALGORITHM_RTREE_H
 
+#include "hit.h"
+#include <vector>
+
+using namespace std;
+
+/*
+ * R-Tree Data structure
+ * spatial indexing of data
+ */
 
 class RTree {
+public:
+    /*
+     * Insert Function
+     * add all data to the data structure,
+     * Using recursive function be aware of the stack used and the stack available
+     */
 
+    void insert(vector<vector<hit>> data){
+
+    }
 };
 
 
