495e7750fdde4ff3c2aba005e452bcd1b6f62005
Marc Aynes <marcaynes2@gmail.com>
2022-03-09 18:53:43 +0100

    add: data to leaf node full, euclidian distance calculation O(N^2)

--- RTree.h
+++ RTree.h
@@ -7,6 +7,7 @@
 #include "hit.h"
 #include <vector>
 #include <float.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -34,6 +35,13 @@ typedef struct leafNode{
 class RTree {
 private:
     Node root{};
+
+    double euclideanDistance(double x1, double y1, double x2, double y2)
+    {
+        // Calculating distance
+        return sqrt(pow(x2 - x1, 2) +
+                    pow(y2 - y1, 2));
+    }
 
     /*
      * Recursive function where we will insert the leaf Node
@@ -85,8 +93,56 @@ private:
                     }
                      */
                 }
-            }else{      // there is not space space
+            }else{      // there is not space
+                int index1 = 0, index2 = 0;
+                double dist = DBL_MAX;
+                for (int i = 0; i < current->numNode; i++) {     //O(N^2) cost at finding 2 furthest points, but for 6 points it's not critical
+                    for (int j = i + 1; j < current->numNode; j++) {
+                        double auxDist = euclideanDistance(((leafNode *) current->node)[i].X,
+                                                           ((leafNode *) current->node)[i].Y,
+                                                           ((leafNode *) current->node)[j].X,
+                                                           ((leafNode *) current->node)[j].Y);
+                        if (auxDist < dist) {
+                            index1 = i;
+                            index2 = j;
+                            dist = auxDist;
+                        }
+                    }
+                }
 
+                /*current->numNode++;
+                current->node = (leafNode *) realloc(current->node, sizeof(leafNode) * (current->numNode));
+                ((leafNode *) (current->node))[(current->numNode) - 1] = leaf;
+
+                if (current->xMax < leaf.X) {   //modifying X and Y data if needed
+                    current->xMax = leaf.X;
+                    return;
+                }
+
+                if (current->xMin > leaf.X) {
+                    current->xMin = leaf.X;
+                    return;
+                }
+
+                if (current->yMax < leaf.Y) {
+                    current->yMax = leaf.Y;
+                    return;
+                }
+
+                if (current->yMin > leaf.Y) {
+                    current->yMin = leaf.Y;
+                    return;
+                }*/
+
+                /*                                                  some prints for debug
+                if (current->numNode >= 3){
+                    int a = 0;
+                    while (a < current->numNode){
+                        printf("id - %d\n", ((leafNode*)current->node)[a].Hit.id);
+                        a++;
+                    }
+                }
+                 */
             }
         }else{          //it is not leaf node
             double newArea = DBL_MAX;
@@ -123,7 +179,7 @@ private:
             }
             insert(leaf, &(((Node*) current->node)[insertIndex]));
             //back propagation
-            
+
         }
     }
 
