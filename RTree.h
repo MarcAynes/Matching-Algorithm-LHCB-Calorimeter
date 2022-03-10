305f566b2627ba8648119de0ef738768bd79d0f8
Marc Aynes <marcaynes2@gmail.com>
2022-03-10 18:29:53 +0100

    add: insertion when we have no space

--- RTree.h
+++ RTree.h
@@ -18,7 +18,7 @@ using namespace std;
 
 typedef struct Node{
     void* node;
-    void* parentNode;
+    Node* parentNode;
     double xMax;
     double yMax;
     double xMin;
@@ -52,36 +52,35 @@ private:
         if (current->leaf){                 //root is leaf
             if (current->numNode < M){      //if there is space
                 if (current->numNode == 0){ //it does not have info insered
-                    current->node = (leafNode*) malloc(sizeof (leafNode));
                     current->numNode++;
-                    *((leafNode*) (current->node)) = leaf;
+                    ((leafNode*) (current->node))[0].Hit = leaf.Hit;
+                    ((leafNode*) (current->node))[0].X = leaf.X;
+                    ((leafNode*) (current->node))[0].Y = leaf.Y;
                     current->yMin = leaf.Y; //adding X and Y data
                     current->yMax = leaf.Y;
                     current->xMin = leaf.X;
                     current->xMax = leaf.X;
                 }else {                     //does have info inserted
                     current->numNode++;
-                    current->node = (leafNode *) realloc(current->node, sizeof(leafNode) * (current->numNode));
-                    ((leafNode *) (current->node))[(current->numNode) - 1] = leaf;
+                    //copying leaf data to the final leafNode
+                    ((leafNode *) (current->node))[(current->numNode) - 1].Hit = leaf.Hit;
+                    ((leafNode *) (current->node))[(current->numNode) - 1].X = leaf.X;
+                    ((leafNode *) (current->node))[(current->numNode) - 1].Y = leaf.Y;
 
                     if (current->xMax < leaf.X) {   //modifying X and Y data if needed
                         current->xMax = leaf.X;
-                        return;
                     }
 
                     if (current->xMin > leaf.X) {
                         current->xMin = leaf.X;
-                        return;
                     }
 
                     if (current->yMax < leaf.Y) {
                         current->yMax = leaf.Y;
-                        return;
                     }
 
                     if (current->yMin > leaf.Y) {
                         current->yMin = leaf.Y;
-                        return;
                     }
 
                     /*                                                  some prints for debug
@@ -94,11 +93,17 @@ private:
                     }
                      */
                 }
-            }else{      // there is not space
-                int index1 = 0, index2 = 0;         //2 farthest points in the rectangle
+            }else{      // there is no space
+                //adding leaf to the 6 temporally position we insert it to iterate over it.
+                ((leafNode*) current->node)[current->numNode].Hit = leaf.Hit;
+                ((leafNode*) current->node)[current->numNode].X = leaf.X;
+                ((leafNode*) current->node)[current->numNode].Y = leaf.Y;
+                int index1 = 0, index2 = 0;
+
+                //find the 2 farthest points in the rectangle
                 double dist = DBL_MAX;
-                for (int i = 0; i < current->numNode; i++) {     //O(N^2) cost at finding 2 furthest points, but for 6 points it's not critical
-                    for (int j = i + 1; j < current->numNode; j++) {
+                for (int i = 0; i <= current->numNode; i++) {     //O(N^2) cost at finding 2 furthest points, but for 6 points it's not critical
+                    for (int j = i + 1; j <= current->numNode; j++) {
                         double auxDist = euclideanDistance(((leafNode *) current->node)[i].X,
                                                            ((leafNode *) current->node)[i].Y,
                                                            ((leafNode *) current->node)[j].X,
@@ -111,10 +116,12 @@ private:
                     }
                 }
 
-                double dist1 = DBL_MAX, ind1 = 0; //2 elements closest to index1 both used to create the minimum rectangle
-                double dist2 = DBL_MAX, ind2 = 0;
+                //find the 2 elements closest to index1 both used to create the minimum rectangle
+                double dist1 = DBL_MAX;
+                double dist2 = DBL_MAX;
+                int ind1 = 0, ind2 = 0;
 
-                for (int i = 0; i < current->numNode; i++){ //linear search for 2 elements closest to index1
+                for (int i = 0; i <= current->numNode; i++){ //linear search for 2 elements closest to index1
                     if (i == index1 || i == index2){
                         continue;
                     }
@@ -127,6 +134,7 @@ private:
                         }
                         dist1 = auxDist;
                         ind1 = i;
+                        continue;
                     }
 
                     if (auxDist < dist2){
@@ -135,45 +143,104 @@ private:
                     }
                 }
 
-                if (current->parentNode == nullptr){
+                if (current->parentNode == nullptr){ // if the Node it's root
+                    leafNode* auxNodes = (leafNode*) current->node;
+                    current->node = nullptr;
+                    current->node = (Node*) malloc(sizeof(Node)*(M+1)); //split, inserting 2 Nodes alloc all M+1 nodes because if we doa realloc we can lose the parent*
+                    current->numNode = 2;
+                    current->leaf = false;
 
-                }else{
-
-                }
-
-                /*current->numNode++;
-                current->node = (leafNode *) realloc(current->node, sizeof(leafNode) * (current->numNode));
-                ((leafNode *) (current->node))[(current->numNode) - 1] = leaf;
-
-                if (current->xMax < leaf.X) {   //modifying X and Y data if needed
-                    current->xMax = leaf.X;
-                    return;
-                }
-
-                if (current->xMin > leaf.X) {
-                    current->xMin = leaf.X;
-                    return;
-                }
-
-                if (current->yMax < leaf.Y) {
-                    current->yMax = leaf.Y;
-                    return;
-                }
-
-                if (current->yMin > leaf.Y) {
-                    current->yMin = leaf.Y;
-                    return;
-                }*/
-
-                /*                                                  some prints for debug
-                if (current->numNode >= 3){
-                    int a = 0;
-                    while (a < current->numNode){
-                        printf("id - %d\n", ((leafNode*)current->node)[a].Hit.id);
-                        a++;
+                    //find the 2 other points which they will make the second rectangle
+                    int ind3 = -1, ind4 = -1;
+                    for (int i = 0; i < M + 1; i++){
+                        if ((i == index1 || i == index2 || i == ind2 || i == ind1)) {
+                            if (ind3 < 0) {
+                                ind3 = i;
+                            }else{
+                                ind4 = i;
+                                break;
+                            }
+                        }
                     }
+
+                    //for each new rectangle insert m (3) leaf nodes
+                    for (int j = 0; j < current->numNode; j++) {
+                        //initializing each new Node
+                        ((Node*) current->node)[j].yMin = DBL_MIN;
+                        ((Node*) current->node)[j].xMin = DBL_MIN;
+                        ((Node*) current->node)[j].xMax = DBL_MAX;
+                        ((Node*) current->node)[j].yMax = DBL_MAX;
+                        ((Node*) current->node)[j].node = (leafNode*) malloc(sizeof(leafNode)*(M + 1));
+                        ((Node*) current->node)[j].numNode = 0;
+                        ((Node*) current->node)[j].leaf = true;
+                        ((Node*) current->node)[j].parentNode = current;
+
+                        //adding working data of the leaf nodes
+                        for (int i = 0; i < m; i++) {
+                            int ind;
+                            switch (j) {
+                                case 0:
+                                    switch (i) {
+                                        case 0:
+                                            ind = index1;
+                                            break;
+                                        case 1:
+                                            ind = ind1;
+                                            break;
+                                        case 2:
+                                            ind = ind2;
+                                            break;
+                                        default:
+                                            ind = index1;
+                                            break;
+                                    }
+                                    break;
+                                default:
+                                    switch (i) {
+                                        case 0:
+                                            ind = index2;
+                                            break;
+                                        case 1:
+                                            ind = ind3;
+                                            break;
+                                        case 2:
+                                            ind = ind4;
+                                            break;
+                                        default:
+                                            ind = index2;
+                                            break;
+                                    }
+                                    break;
+
+                            }
+
+                            //copying each leaf node to it's rectangle (Node)
+                            ((Node*) current->node)[j].numNode++; //rectangle have +1 leaf node (data point)
+                            ((leafNode*) ((Node*) current->node)[j].node)[((((Node*) current->node)[i].numNode) - 1)].Hit = auxNodes[ind].Hit; //adding hit data to it's rectangle (Node)
+                            ((leafNode*) ((Node*) current->node)[j].node)[((((Node*) current->node)[i].numNode) - 1)].X = auxNodes[ind].X;
+                            ((leafNode*) ((Node*) current->node)[j].node)[((((Node*) current->node)[i].numNode) - 1)].Y = auxNodes[ind].Y;
+
+                            //modifying rectangle boundaries if needed
+                            if (((Node*) current->node)[j].xMax < auxNodes[ind].X) {
+                                ((Node*) current->node)[j].xMax = auxNodes[ind].X;
+                            }
+
+                            if (((Node*) current->node)[j].xMin > auxNodes[ind].X) {
+                                ((Node*) current->node)[j].xMin = auxNodes[ind].X;
+                            }
+
+                            if (((Node*) current->node)[j].yMax < auxNodes[ind].Y) {
+                                ((Node*) current->node)[j].yMax = auxNodes[ind].Y;
+                            }
+
+                            if (((Node*) current->node)[j].yMin > auxNodes[ind].Y) {
+                                ((Node*) current->node)[j].yMin = auxNodes[ind].Y;
+                            }
+                        }
+                    }
+                }else{  //node is not root
+
                 }
-                 */
             }
         }else{          //it is not leaf node
             double newArea = DBL_MAX;
@@ -235,7 +302,7 @@ public:
 
     RTree(){
         root.leaf = true;
-        root.node = nullptr;
+        root.node = (leafNode*) malloc(sizeof(leafNode)*(M + 1));   //alloc all memory, if we don't do this when we do a realloc on a Node we may lose the parent* node
         root.numNode = 0;
         root.xMax = -1;
         root.xMin = -1;
